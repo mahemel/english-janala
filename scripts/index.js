2b80c6fdd870ea51ae31f009cf46d06dccfc0bb7
mahemel <mahbubhasanhemel@gmail.com>
2026-03-04 00:05:56 +0600

    words are now dynamic

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,64 @@ const loadLessons = async () => {
             displayLesson(json.data);
         })
 }
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    console.log(url)
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            displayLevelWord(json.data)
+        })
+}
+
+const displayLevelWord = (levelWords) => {
+
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML = '';
+
+    /*
+{
+    "id": 60,
+    "level": 3,
+    "word": "Jubilant",
+    "meaning": "আনন্দে উৎফুল্ল",
+    "pronunciation": "জুবিলান্ট"
+}
+
+
+    */
+
+    if(levelWords.length === 0) {
+        wordContainer.innerHTML = `
+        <div class="text-center col-span-full font-bangla space-y-2">
+            <img src="assets/alert-error.png" class="mx-auto" alt="" />
+            <p class="font-medium text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="text-xl font-bold">নেক্সট Lesson এ যান</h2>
+        </div>        
+        `;
+
+        return;
+    }
+
+    levelWords.forEach(word => {
+
+        const card = document.createElement('div');
+        card.innerHTML = `
+        <div class="bg-white text-center rounded-xl shadow-sm py-8 px-6 space-y-4">
+            <h2 class="text-2xl font-bold">${word.word}</h2>
+            <p>Meaning /Pronounciation</p>
+            <div class="font-bangla font-semibold text-xl">"${word.meaning} / ${word.pronunciation}"</div>
+
+            <div class="flex justify-between items-center">
+                <button class="btn btn-square bg-[#1A91FF]/10 hover:bg-[#1A91FF]/40" type="button"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn btn-square bg-[#1A91FF]/10 hover:bg-[#1A91FF]/40" type="button"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        `;
+
+        wordContainer.appendChild(card)
+    })
+}
 
 const displayLesson = (lessons) => {
     const levelContainer = document.getElementById('level-container');
@@ -18,11 +76,11 @@ const displayLesson = (lessons) => {
         const btnDiv = document.createElement('div');
 
         btnDiv.innerHTML = `
-            <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>            
+            <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>            
         `;
 
         levelContainer.appendChild(btnDiv)
     })
 }
 
-loadLessons()
+loadLessons();
